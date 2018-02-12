d7f70a9be583807655437d781752f29c3cffeeb7
flask <shuo502@163.com>
2018-02-12 16:54:26 +0800

    8080

---
+++ wsgi.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: ??
+@author: mum
+@license: Apache Licence 
+@contact: shuo502@163.com
+@author: ‘yo‘
+@site: http://github.com/shuo502
+@software: PyCharm
+@file: wsgi.py.py
+@time: 2018/2/12 16:28
+"""
+from app import app as application
+
+if __name__ == "__main__":
+    application.run()
