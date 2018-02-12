40663b6c2601f5cd72791b8640c1842b582d7005
flask <shuo502@163.com>
2018-02-12 15:42:30 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: ??
+@author: mum
+@license: Apache Licence 
+@contact: shuo502@163.com
+@author: ‘yo‘
+@site: http://github.com/shuo502
+@software: PyCharm
+@file: setup.py.py
+@time: 2018/2/12 15:36
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
+
+
