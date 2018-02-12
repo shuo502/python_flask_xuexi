c9ea703af8332a464143720a5f2e93f5ba76aebf
flask <shuo502@163.com>
2018-02-12 16:47:50 +0800

    8080

--- app.py
+++ app.py
@@ -13,3 +13,7 @@ def run():
 
 if __name__ == '__main__':
     app.run(port=8080)
+# pip install pyOpenSSL
+# 1
+# 2 在 Flask 的代码中可以直接使用
+    app.run('0.0.0.0', debug=True, port=8100, ssl_context='adhoc')
